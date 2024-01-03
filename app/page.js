a84c85eca263d0094bb398ab64c5ce9d41dc09ae
afhay <afhay916@gmail.com>
2024-01-03 19:05:50 +0700

    interface new did

--- app/page.js
+++ app/page.js
@@ -1,25 +1,31 @@
 "use client";
 
+import toast, { Toaster } from "react-hot-toast";
 import { Icon } from "@iconify/react";
 import Image from "next/image";
-import { useState } from "react";
-
-// const identities = [
-//   { did: "1111111111", name: "First" },
-//   { did: "2222222222", name: "Second" },
-//   { did: "3333333333", name: "Third" },
-//   { did: "4444444444", name: "Fourth" },
-//   { did: "5555555555", name: "Fifth" },
-//   { did: "6666666666", name: "Sixth" },
-//   { did: "7777777777", name: "Seventh" },
-// ];
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 export default function Home() {
+  const walletNameRef = useRef();
+  let [isOpen, setIsOpen] = useState(true);
   const [dids, setDids] = useState([]);
+
+  const handleNewDid = async () => {
+    toast("new DID created!", {
+      position: "bottom-center",
+      icon: "🚀",
+      style: {
+        borderRadius: "10px",
+        background: "#333",
+        color: "#fff",
+      },
+    });
+  };
 
   return (
     <main className="h-screen bg-zinc-950 text-white p-0 sm:p-8">
-      <div className="w-full flex flex-col sm:max-w-md mx-auto h-full rounded-xl bg-zinc-900 border border-zinc-800 py-4 pl-4">
+      <div className="w-full flex flex-col sm:max-w-md mx-auto h-full rounded-none sm:rounded-xl bg-zinc-900 border border-zinc-800 py-4 pl-4">
         <div className="flex items-center justify-between mb-4 shrink-0 pr-4">
           <div className="flex items-center gap-2">
             <Icon icon="heroicons:finger-print-16-solid" className="w-7 h-7" />
@@ -27,7 +33,10 @@ export default function Home() {
               Iden<span className="text-orange-500">did</span>y
             </h1>
           </div>
-          <button className="bg-orange-600 flex items-center gap-1 px-4 py-2 text-sm font-semibold rounded-md hover:bg-orange-700 transition-all">
+          <button
+            className="bg-orange-600 flex items-center gap-1 px-4 py-2 text-sm font-semibold rounded-md hover:bg-orange-700 transition-all"
+            onClick={() => setIsOpen(true)}
+          >
             <Icon icon="heroicons:plus-16-solid" className="w-4 h-4" />
             New DID
           </button>
@@ -54,7 +63,10 @@ export default function Home() {
                 <Image src="/img/looking.webp" fill alt="Looking" />
               </div>
               <p className="font-medium text-lg mt-4 mb-4">You don&apos;t have a DID</p>
-              <button className="bg-orange-600 flex items-center gap-1 px-4 py-2 text-sm font-semibold rounded-md hover:bg-orange-700 transition-all">
+              <button
+                className="bg-orange-600 flex items-center gap-1 px-4 py-2 text-sm font-semibold rounded-md hover:bg-orange-700 transition-all"
+                onClick={() => setIsOpen(true)}
+              >
                 <Icon icon="heroicons:plus-16-solid" className="w-4 h-4" />
                 New DID
               </button>
@@ -62,6 +74,65 @@ export default function Home() {
           )}
         </div>
       </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-zinc-900">
+                    Create new DID
+                  </Dialog.Title>
+
+                  <div className="mt-2">
+                    <input type="text" ref={walletNameRef} className="w-full border border-gray-300 rounded-md px-4 py-2" placeholder="Wallet name" />
+                  </div>
+
+                  <div className="mt-4 flex items-center gap-2">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white hover:bg-orange-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 transition-all"
+                      onClick={() => handleNewDid()}
+                    >
+                      Submit
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-orange-100 px-4 py-2 text-sm font-medium text-orange-900 hover:bg-orange-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus-visible:ring-offset-2 transition-all"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+
+      <Toaster />
     </main>
   );
 }
